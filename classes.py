445b23f4e4a95a211a0ece971fb158cd0b6924a1
William Butts <wbutts1023@gmail.com>
2016-11-10 21:29:41 -0600

    Starting with basic, unfinished classes.

---
+++ classes.py
@@ -0,0 +1,21 @@
+
+
+class Player():
+
+    def __init__(self, starting_location):
+        self.location = starting_location
+        self.inventory = [,]
+        
+
+
+class Room(self): 
+    
+    def __init__(self, name, exits, items):
+        self.exits = exits
+        self.items = items
+    
+class Item():
+
+    def __init__(self, name):
+        self.name = name
+        
