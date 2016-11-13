dd1552b3f467db276200b052aa3abc1a667462cd
William Butts <wbutts1023@gmail.com>
2016-11-12 18:59:49 -0600

    added the bones of a first game, needs the play loop but the set up should be done

---
+++ basic_game.py
@@ -0,0 +1,11 @@
+
+from classes.py import *
+
+room1 = Room('Room 1', 'This is the basic starting room, any bugs?')
+room2 = Room('Room 2', 'It\'s real dark in here, got any light?')
+room1.add_exit('North', room2)
+room2.add_exit('South', room1)
+room1.add_item('A torch', 'shining brightly, maybe lights up something?', room2)
+player = Player(room1)
+
+
