b2f008ddab17992a86892b2e24fd7661d3237cf7
William Butts <wbutts1023@gmail.com>
2016-11-15 23:14:47 -0700

    added a fail case to the parser

--- text_parser.py
+++ text_parser.py
@@ -8,14 +8,16 @@ def parse_text(player, room):
     if re.search('move',text) or re.search('go',text)::
         player.move_rooms(text)
     #take item
-    if re.search('take',text) or re.search('get',text):
+    elif re.search('take',text) or re.search('get',text):
         player.get_item(text)
     #look rooms/items
-    if re.search('look', text) or re.search('view', text):
+    elif re.search('look', text) or re.search('view', text):
         if len(text) == 4:
             player.view_room()
         else:
             player.view_item(text)
     #use item
-    if re.search('use',text):
+    elif re.search('use',text):
         player.use_item(text)
+    else:
+        print('I didn\'t understand that.  Please start your statement with move, get, look, or use')
