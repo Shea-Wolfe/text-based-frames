cfd01e0081ac9c7e6343066c9f718b4f4106cef3
William Butts <wbutts1023@gmail.com>
2016-11-20 22:01:33 -0600

    added basic item generation

--- generation.py
+++ generation.py
@@ -5,18 +5,19 @@ def generation_loop():
     whlie True:
         text = input('please (C)reate, (E)dit, or  (D)one').lower()
         if text == 'c' or text == 'create':
-            creation_text = input('Would you like to make\nA (R)oom\nAdd an (I)tem to an existing room\nAdd a (S)olution to a room/item combo\nAn (E)xit between two rooms\nGo (B)ack to the previous menu').lower()
-            if creation_text == 'r' or creation_text == 'room':
-                room = generate_room()
-                rooms[room.name] = room
-            elif creation_text == 'i' or creation_text == 'item':
-                items.append(generate_item())
-            elif creation_text == 's' or creation_text == 'solution':
-                generate_solution()
-            elif creation_text == 'e' or creation_text == 'exit':
-                generate_exit()
-            elif creation_text == 'b' or creation_text == 'back':
-                break
+            while True:
+                creation_text = input('Would you like to make\nA (R)oom\nAdd an (I)tem to an existing room\nAdd a (S)olution to a room/item combo\nAn (E)xit between two rooms\nGo (B)ack to the previous menu').lower()
+                if creation_text == 'r' or creation_text == 'room':
+                    room = generate_room()
+                    rooms[room.name] = room
+                elif creation_text == 'i' or creation_text == 'item':
+                    items.append(generate_item())
+                elif creation_text == 's' or creation_text == 'solution':
+                    generate_solution()
+                elif creation_text == 'e' or creation_text == 'exit':
+                    generate_exit()
+                elif creation_text == 'b' or creation_text == 'back':
+                    break
         elif text == 'e' or text == 'edit':
             #edit rooms/items
         elif text == 'd' or text == 'done':
@@ -30,9 +31,26 @@ def generate_room():
     while True:
         items = input('Would you like to add an item to the room? Y/N').lower()
         if items == 'y' or items == 'yes':
-            item = generate_item()
+            item = generate_item(room)
             room.items[item.name] = item
         else:
             break
-    
+    return room 
+
+def generate_item(room=None):
+    name = input('Please enter the name of the item').lower()
+    view = input('Please enter what the item looks like in the room').lower()
+    description = input('Please enter what the item looks like in the player inventory').lower()
+    if room:
+        room.add_item(name, description, view)
+    else:
+        while True:
+            room = input('Please enter the name of the room this item is in').lower()
+            if room in rooms:
+                room = rooms[room]
+                return item
+            elif room == 'exit' or room == 'quit'
+                break
+            else:
+                print('That is not a valid room')
 
