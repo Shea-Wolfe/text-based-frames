899b05f4b7f46f688c4d31948a608f9234565d12
William Butts <wbutts1023@gmail.com>
2016-11-20 21:42:51 -0600

    have a basic room generation with bonus item generation text

--- generation.py
+++ generation.py
@@ -7,13 +7,14 @@ def generation_loop():
         if text == 'c' or text == 'create':
             creation_text = input('Would you like to make\nA (R)oom\nAdd an (I)tem to an existing room\nAdd a (S)olution to a room/item combo\nAn (E)xit between two rooms\nGo (B)ack to the previous menu').lower()
             if creation_text == 'r' or creation_text == 'room':
-                #make a room
+                room = generate_room()
+                rooms[room.name] = room
             elif creation_text == 'i' or creation_text == 'item':
-                #make an item in an existing room
+                items.append(generate_item())
             elif creation_text == 's' or creation_text == 'solution':
-                #added solution elements to an existing room/item
+                generate_solution()
             elif creation_text == 'e' or creation_text == 'exit':
-                #create an exit link for two rooms
+                generate_exit()
             elif creation_text == 'b' or creation_text == 'back':
                 break
         elif text == 'e' or text == 'edit':
@@ -22,4 +23,16 @@ def generation_loop():
             #write the file
 
             
+def generate_room():
+    name = input('Please enter the name of the room').lower()
+    description = input('Please enter what the player sees when they enter or view the room').lower()
+    room = Room(name, description)
+    while True:
+        items = input('Would you like to add an item to the room? Y/N').lower()
+        if items == 'y' or items == 'yes':
+            item = generate_item()
+            room.items[item.name] = item
+        else:
+            break
     
+
