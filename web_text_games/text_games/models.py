3e802c252eb5f9c7e0039827eb5c372d0437ff2c
William Butts <wbutts1023@gmail.com>
2016-12-25 23:20:59 -0600

    set up some basic foreignkeys, using related names to double up

--- web_text_games/text_games/models.py
+++ web_text_games/text_games/models.py
@@ -9,6 +9,10 @@ class Item(models.Model):
     name = models.CharField(max_length=140)
     view = models.CharField(max_length=140)
     description = models.CharField(max_length=140)
+    room = models.ForeignKey(Room)
 
 class Exit(models.Model):
-    pass
+    first_room = models.ForeignKey(Room, related_name='first_room')
+    second_room = models.ForeignKey(Room, related_name='second_room')
+
+
