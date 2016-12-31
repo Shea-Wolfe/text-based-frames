b472bed9b8b861239c7debad6b0f686f033f9106
William Butts <wbutts1023@gmail.com>
2016-12-30 22:43:34 -0600

    added a game line to rooms and exits to allow for filtering rooms by game, and dynamic rooms

--- web_text_games/text_games/models.py
+++ web_text_games/text_games/models.py
@@ -4,10 +4,11 @@ class Game(models.Model):
     save_name = models.CharField(max_length=140)
     unique_id = models.IntegerField(null=True)
     player_name = models.CharField()
-
+    
 class Room(models.Model):
     name = models.CharField(max_length=140)
     description = models.CharField(max_length=140)
+    game = models.ForeignKey(Game)
 
 class Item(models.Model):
     name = models.CharField(max_length=140)
@@ -19,5 +20,6 @@ class Exit(models.Model):
     name = models.CharField(max_length=140)
     exit = models.ForeignKey(Room, related_name='exit')
     entrance = models.ForeignKey(Room, related_name='entrance')
+    game = models.ForeignKey(Game)
 
 
