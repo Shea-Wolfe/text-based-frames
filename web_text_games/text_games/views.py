451e336b1264b81d5eb8e94b0beda3ad7425d4cb
William Butts <wbutts1023@gmail.com>
2017-01-02 22:33:24 -0600

    grabbed user creation off an old project, needs testing

--- web_text_games/text_games/views.py
+++ web_text_games/text_games/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+regster_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                user = authenticate(username=user.username,
+                                    password=request.POST['password1'])
+                login(request, user)
+                #Send the user to a game here
+    else:
+        form = UserCreationForm()
+    return render(request, 'text_games/register.html', {'form':form}) 
